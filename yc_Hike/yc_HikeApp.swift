ac811bfa44d3960b93d7f30e49715cbbb3455d1a
prettyturtle <unifconti@gmail.com>
2023-08-02 21:51:53 +0900

    Initial Commit

---
+++ yc_Hike/yc_HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_HikeApp.swift
+//  yc_Hike
+//
+//  Created by yc on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct yc_HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
